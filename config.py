4264dbee813bb08f04d63262f19b8d396f79961c
Zoroooooo0 <keshavb1980@gmail.com>
2024-09-01 08:45:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7533718787:AAHe2ecT2IYZvxJCsOSsyg7onBz3mnuLqEM")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6395916793")
 
 
 
